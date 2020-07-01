06d04140bb60bd8e55d4f47830f424b42e062b79
rajeev95829 <r.singh95829@gmail.com>
2020-07-01 17:58:57 +0530

    Added new native query for new function.

--- src/main/java/com/example/java_api/repository/EmployeesRepository.java
+++ src/main/java/com/example/java_api/repository/EmployeesRepository.java
@@ -1,5 +1,6 @@
 package com.example.java_api.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,4 +14,8 @@ public interface EmployeesRepository extends JpaRepository<Employees, Integer>{
 	@Query(value="select * from employees where emp_no=:emp_no", nativeQuery=true)
 	
 	public Optional<Employees> getById(@Param("emp_no") int emp_no);
+	
+	@Query(value="select * from employees where first_name like ?1% ", nativeQuery=true)
+	
+	public List<Employees>getByName(@Param("first_name") String Ra);
 }
