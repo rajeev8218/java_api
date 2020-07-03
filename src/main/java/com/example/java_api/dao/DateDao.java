d97170e2505dbda87f39bc39071134906701c423
rajeev95829 <r.singh95829@gmail.com>
2020-07-03 17:54:36 +0530

    Added new model which belongs to employees model's functionality.

---
+++ src/main/java/com/example/java_api/dao/DateDao.java
@@ -0,0 +1,25 @@
+package com.example.java_api.dao;
+
+public class DateDao {
+
+	private String fromdate;
+	
+	private String todate;
+	
+	public String getFromdate() {
+		return fromdate;
+	}
+
+	public void setFromdate(String fromdate) {
+		this.fromdate = fromdate;
+	}
+
+	public String getTodate() {
+		return todate;
+	}
+
+	public void setTodate(String todate) {
+		this.todate = todate;
+	}
+	
+}
